5b9ebd7ebef8ae33126ed41bf0ea7df35893382a
dongxiaohui2008 <gongzuo303@126.com>
2015-08-26 15:52:43 +0800

    hui

---
+++ Test/Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by EntGroup on 15/8/26.
+//  Copyright (c) 2015年 HH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test/Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by EntGroup on 15/8/26.
+//  Copyright (c) 2015年 HH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
